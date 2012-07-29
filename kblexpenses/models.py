af79be3f3d5c01651a5ab2d2d32154f2f5a1c603
Hartley Brody <hbrody@hubspot.com>
2012-07-28 21:07:29 -0400

    first pass at models

--- kblexpenses/models.py
+++ kblexpenses/models.py
@@ -1,4 +1,26 @@
 from google.appengine.ext import db
 
-class Candidate(db.Model):
-    pass
+class Resident(db.Model):
+    name = db.StringProperty()
+    is_current_resident = db.BooleanProperty(default=True)
+    email_address = db.EmailProperty()
+    
+class Expense(db.Model):
+    paid_by = db.ReferenceProperty(Resident)
+    cost = db.FloatProperty()
+    date = db.DateProperty()
+    title = db.StringProperty()
+    memo = db.TextProperty()
+    
+class Request(db.Model):
+    """an explicit request for money from one resident to another"""
+    expense = db.ReferenceProperty(Expense)
+    to = db.ReferenceProperty(Resident)
+    amount = db.FloatProperty()
+    
+class Payment(db.Model):
+    """an indication that someone has paid off a request"""
+    request = db.ReferenceProperty(Request)
+    amount = db.FloatProperty()
+    datetime = db.DateTimeProperty()
+    acknowleged = db.BooleanProperty()
