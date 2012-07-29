eb4924e92ef3d599cdb382430f3d08fe110f2d6e
Hartley Brody <hbrody@hubspot.com>
2012-07-29 00:24:45 -0400

    flehed out API

--- kblexpenses/main.py
+++ kblexpenses/main.py
@@ -1,19 +1,4 @@
 #!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import webapp2
 import logging
 try:
@@ -27,15 +12,11 @@ from google.appengine.ext.webapp import template
 from google.appengine.api import users
 
 class MainHandler(webapp2.RequestHandler):
+    # homepage,  handles auth and templates
     def get(self):
-        """
-            main page. mostly handles authenticating
-            the user and setting up the necessary templates
-        """
         current_user = users.get_current_user()
         if not current_user:
-            self.error(403)
-            return
+            self.error(403); return
         
         e = current_user.email()
         try:
@@ -43,28 +24,70 @@ class MainHandler(webapp2.RequestHandler):
         except IndexError:
             logging.info("%s tried to login" % e)
             self.error(403)
-            self.response.out.write("<h3>Couldn't find a resident with email address:</h3> %s" % e)
+            self.response.out.write("<h3>Couldn't find a resident with email address: %s</h3>" % e)
             return
             
         template_values = { 
             "resident": current_resident,
             "admin": users.is_current_user_admin()
         } 
-        self.response.out.write(template.render("templates/create_review.html", template_values))
+        self.response.out.write(template.render("templates/index.html", template_values))
             
 class ResidentApiHandler(webapp2.RequestHandler):
     def get(self):
+        """
+            TODO: remove this once the 5 residents are in the db
+        """
         name = self.request.get("name")
         email = self.request.get("email")
+        gender = self.request.get("gender")
         
-        r = Resident(name=name, email=email)
+        r = Resident(name=name, email=email, gender=gender)
         r.put()
         
-        self.response.out.write("successfully created resident with name [%s] and email [%s]"% (name, email))
+        self.response.out.write("created resident with name [%s] and email [%s]"% (name, email))
+        
+class ExpenseApiHandler(webapp2.RequestHandler):
+    def post(self):
+        pay_to = self.request.get("pay_to")
+        owner = self.request.get("owner_id")
+        title = self.request.get("title")
+        cost = self.request.get("cost")
+        date = self.request.get("date")
+        memo = self.request.get("memo")
+        
+        e = Expense(owner=owner, title=title, cost=cost, date=date, memo=memo)
+        e.put()
+        
+        self.response.out.write("")
+        
+class RequestApiHandler(webapp2.RequestHandler):
+    def post(self):
+        expense = self.request.get("expense_id")
+        recipient = self.request.get("recipient_id")
+        amount = self.request.get("amount")
+        
+        r = Request(expense=expense, recipient=recipient, amount=amount)
+        r.put()
+        
+        self.response.out.write("")
+        
+class PaymentApiHandler(webapp2.RequestHandler):
+    def post(self):
+        amount = request.get("amount")
+        request = request.get("request_id") # NB: this breaks the "request" var
+        
+        p = Payment(request=request, amount=amount)
+        p.put()
+        
+        self.response.out.write("")
 
 app = webapp2.WSGIApplication([
         ('/',               MainHandler),
         ('/api/resident/',  ResidentApiHandler),
+        ('/api/expense/',   ExpenseApiHandler),
+        ('/api/request/',   RequestApiHandler),
+        ('/api/payment/',   PaymentApiHandler),
     ], debug=True)
     
 def main():
--- kblexpenses/models.py
+++ kblexpenses/models.py
@@ -4,23 +4,27 @@ class Resident(db.Model):
     name = db.StringProperty()
     is_current_resident = db.BooleanProperty(default=True)
     email = db.EmailProperty()
+    gender = db.StringProperty()
     
 class Expense(db.Model):
-    paid_by = db.ReferenceProperty(Resident)
+    owner = db.ReferenceProperty(Resident)
     cost = db.FloatProperty()
     date = db.DateProperty()
     title = db.StringProperty()
+    pay_to = db.StringProperty()
     memo = db.TextProperty()
+    is_valid = db.BooleanProperty(default=True) # allow cancelled expenses
     
 class Request(db.Model):
     """an explicit request for money from one resident to another"""
     expense = db.ReferenceProperty(Expense)
-    to = db.ReferenceProperty(Resident)
+    recipient = db.ReferenceProperty(Resident)
     amount = db.FloatProperty()
+    is_valid = db.BooleanProperty(default=True) # allow cancelled requests
     
 class Payment(db.Model):
     """an indication that someone has paid off a request"""
     request = db.ReferenceProperty(Request)
     amount = db.FloatProperty()
-    datetime = db.DateProperty()
-    acknowleged = db.BooleanProperty()
+    date = db.DateProperty(auto_now=True)
+    acknowleged = db.BooleanProperty(default=False)
