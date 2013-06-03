8653c5460368d8da4c6836a5ad8bcca9fbdfe4d0
Hartley Brody <hartley.brody@gmail.com>
2013-06-03 17:46:19 -0400

    remove proxies, choose IP address randomly

--- benchmark.py
+++ benchmark.py
@@ -1,6 +1,6 @@
 import sys
 import csv
-from random import choice
+from random import choice, randint
 from datetime import datetime
 from collections import defaultdict
 from BeautifulSoup import BeautifulSoup
@@ -8,8 +8,8 @@ from BeautifulSoup import BeautifulSoup
 import eventlet
 requests = eventlet.import_patched('requests')
 
-NUM_REQUESTS = 10000
-CONCURRENT_REQUESTS = 50
+NUM_REQUESTS = 100
+CONCURRENT_REQUESTS = 5
 USER_AGENTS = [
     "Mozilla/5.0 (X11; Linux x86_64; rv:12.0) Gecko/20100101 Firefox/21.0",
     "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1468.0 Safari/537.36",
@@ -66,108 +66,23 @@ USER_AGENTS = [
     "Googlebot/2.1 (+http://www.googlebot.com/bot.html)",
     "Googlebot/2.1 (+http://www.google.com/bot.html)",
 ]
-IP_ADDRESSES = [
-    "10.0.0.1",
-    "10.0.0.2",
-    "10.0.0.4",
-    "10.0.0.8",
-    "10.0.0.16",
-    "222.42.1.226",
-    "113.53.232.46",
-    "180.250.129.184",
-    "111.90.146.196",
-    "217.13.118.93",
-    "198.154.114.100",
-    "190.95.199.243",
-    "183.60.126.184",
-    "123.103.23.106",
-    "110.77.226.216",
-    "118.99.126.162",
-    "118.175.88.45",
-    "159.255.166.29",
-    "195.165.58.61",
-    "118.196.121.17",
-    "220.422.125.226",
-    "116.131.232.246",
-    "120.258.129.184",
-    "191.905.146.196",
-    "215.130.118.193",
-    "198.154.114.100",
-    "190.195.199.243",
-    "183.160.126.184",
-    "123.103.223.106",
-    "110.177.226.216",
-    "118.199.126.162",
-    "118.175.188.245",
-    "159.255.166.129",
-    "195.165.158.161",
-    "118.196.121.179",
-]
-
-PROXIES = [
-    "http://222.42.1.226:80",
-    "https://113.53.232.46:3128",
-    "http://180.250.129.184:8080",
-    "http://111.90.146.196:80",
-    "https://217.13.118.93:80",
-    "https://198.154.114.100:8089",
-    "https://190.95.199.243:80",
-    "http://183.60.126.184:1080",
-    "https://123.103.23.106:29786",
-    "https://110.77.226.216:3128",
-    "https://118.99.126.162:8080",
-    "https://118.175.88.45:8080",
-    "https://159.255.166.29:8080",
-    "https://95.65.58.61:443",
-    "https://118.96.121.17:8080",
-    "https://66.35.68.146:3128",
-    "https://80.191.169.66:8080",
-    "http://80.58.29.170:80",
-    "https://124.126.126.7:80",
-    "http://190.128.170.18:8080",
-    "http://211.143.200.183:80",
-    "http://36.32.20.222:8080",
-    "https://58.252.56.149:9000",
-    "https://200.6.121.105:80",
-    "http://122.72.28.118:80",
-    "https://221.13.79.20:80",
-    "https://59.46.67.108:8080",
-    "https://125.39.66.154:80",
-    "http://113.12.83.157:80",
-    "https://116.213.51.80:80",
-    "https://182.48.191.83:8080",
-    "https://117.211.91.147:808",
-    "http://218.108.168.67:82",
-    "https://124.237.92.2:8080",
-    "https://186.103.143.210:8080",
-    "http://200.62.231.218:3128",
-    "https://122.72.0.145:80",
-    "https://93.78.92.179:3128",
-    "http://189.59.17.206:3128",
-    "http://211.167.64.112:8080",
-    "https://178.254.152.102:6666",
-    "https://58.242.249.31:35010",
-    "https://213.16.101.18:3128",
-    "https://180.173.89.208:8080",
-    "https://218.197.148.4:21",
-    "https://2.133.93.178:9090",
-    "https://180.250.144.231:8080",
-    "https://111.67.78.34:80",
-    "http://123.103.23.106:10064",
-]
 
 
 def fetch(url):
+
+    ip_address = "{}.{}.{}.{}".format(
+        randint(10, 255),
+        randint(10, 255),
+        randint(10, 255),
+        randint(10, 255),
+    )
     try:
         headers = {
             "Connection": "close",
             "Cache-Control": "no-cache",
             "User-Agent": choice(USER_AGENTS),
-            "Via": choice(IP_ADDRESSES),
-            "X-Forwarded-For": choice(IP_ADDRESSES),
-        }
-        proxies = {
-            "http": choice(PROXIES)
+            "Via": ip_address,
+            "X-Forwarded-For": ip_address,
         }
 
         # make and time request
@@ -175,8 +90,8 @@ def fetch(url):
         r = requests.get(url, headers=headers)
         end = datetime.now()
 
-        # soup = BeautifulSoup(r.text)
-        # print soup.find("title")
+        soup = BeautifulSoup(r.text)
+        print soup.find("title").text
 
         # how long did it take?
         diff = end - start
@@ -185,7 +100,7 @@ def fetch(url):
         # round to nearest second
         decimal = seconds % 1
         if decimal >= 0.5:
-            return int(seconds) +1
+            return int(seconds) + 1
         else:
             return int(seconds)
 
