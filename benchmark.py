1f51c4ab064982e3282e375d559ed332bad680c9
Hartley Brody <hartley.brody@gmail.com>
2013-06-04 00:00:43 -0400

    broken raw socket code, shamelessly copy/pasted :(

--- benchmark.py
+++ benchmark.py
@@ -1,5 +1,8 @@
 import sys
 import csv
+import socket
+from struct import pack
+
 from random import choice, randint
 from datetime import datetime
 from collections import defaultdict
@@ -7,9 +10,10 @@ from BeautifulSoup import BeautifulSoup
 
 import eventlet
 requests = eventlet.import_patched('requests')
+time = eventlet.import_patched('time')
 
-NUM_REQUESTS = 100
-CONCURRENT_REQUESTS = 5
+NUM_REQUESTS = 2
+CONCURRENT_REQUESTS = 1
 USER_AGENTS = [
     "Mozilla/5.0 (X11; Linux x86_64; rv:12.0) Gecko/20100101 Firefox/21.0",
     "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1468.0 Safari/537.36",
@@ -68,14 +72,114 @@ USER_AGENTS = [
 ]
 
 
-def fetch(url):
+def get_ip():
 
-    ip_address = "{}.{}.{}.{}".format(
+    return "{}.{}.{}.{}".format(
         randint(10, 255),
         randint(10, 255),
         randint(10, 255),
         randint(10, 255),
     )
+
+
+# checksum functions needed for calculation checksum
+def checksum(msg):
+    s = 0
+    # loop taking 2 characters at a time
+    for i in range(0, len(msg), 2):
+        w = (ord(msg[i]) << 8) + (ord(msg[i+1]))
+        s = s + w
+
+    s = (s >> 16) + (s & 0xffff)
+    #s = s + (s >> 16);
+    #complement and mask to 4 byte short
+    s = ~s & 0xffff
+
+    return s
+
+
+def connect(host):
+
+    if host.startswith("http://"):
+        host = host[7:]
+    if host.endswith("/"):
+        host = host[:-1]
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
+
+    # tell kernel not to put in headers, since we'll be providing them
+    s.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+
+    source_ip = get_ip()
+    dest_ip = socket.gethostbyname(host)
+
+    # ip header fields
+    ihl = 5
+    version = 4
+    tos = 0
+    tot_len = 20 + 20   # python seems to correctly fill the total length, dont know how ??
+    id = 54321  # id of this packet
+    frag_off = 0
+    ttl = 255
+    protocol = socket.IPPROTO_TCP
+    check = 10  # python seems to correctly fill the checksum
+    saddr = socket.inet_aton( source_ip )  # Spoof the source ip address if you want to
+    daddr = socket.inet_aton( dest_ip )
+
+    ihl_version = (version << 4) + ihl
+
+    # the ! in the pack format string means network order
+    ip_header = pack('!BBHHHBBH4s4s', ihl_version, tos, tot_len, id, frag_off, ttl, protocol, check, saddr, daddr)
+
+    # tcp header fields
+    source = 1234   # source port
+    dest = 80   # destination port
+    seq = 0
+    ack_seq = 0
+    doff = 5    # 4 bit field, size of tcp header, 5 * 4 = 20 bytes
+    #tcp flags
+    fin = 0
+    syn = 1
+    rst = 0
+    psh = 0
+    ack = 0
+    urg = 0
+    window = socket.htons(5840)  # maximum allowed window size
+    check = 0
+    urg_ptr = 0
+
+    offset_res = (doff << 4) + 0
+    tcp_flags = fin + (syn << 1) + (rst << 2) + (psh << 3) + (ack << 4) + (urg << 5)
+
+    # the ! in the pack format string means network order
+    tcp_header = pack('!HHLLBBHHH', source, dest, seq, ack_seq, offset_res, tcp_flags,  window, check, urg_ptr)
+
+    # pseudo header fields
+    source_address = socket.inet_aton( source_ip )
+    dest_address = socket.inet_aton(dest_ip)
+    placeholder = 0
+    protocol = socket.IPPROTO_TCP
+    tcp_length = len(tcp_header)
+
+    psh = pack('!4s4sBBH', source_address, dest_address, placeholder, protocol, tcp_length)
+    psh = psh + tcp_header
+
+    tcp_checksum = checksum(psh)
+
+    # make the tcp header again and fill the correct checksum
+    tcp_header = pack('!HHLLBBHHH', source, dest, seq, ack_seq, offset_res, tcp_flags,  window, tcp_checksum, urg_ptr)
+
+    # final full packet - syn packets dont have any data
+    packet = ip_header + tcp_header
+
+    #Send the packet finally - the port specified has no effect
+    print dest_ip
+    s.sendto(packet, (dest_ip, 80))    # put this in a loop if you want to flood the target
+
+
+def fetch(url):
+
+    ip_address = get_ip()
     try:
         headers = {
             "Connection": "close",
@@ -114,9 +218,12 @@ def main(site):
     pile = eventlet.GreenPile(pool)
 
     for x in xrange(NUM_REQUESTS):
-        # eventlet.spawn(fetch, [site])
-        pile.spawn(fetch, site)
+        # eventlet.spawn(connect, [site])
+        # pile.spawn(fetch, site)
+        print "adding to pile"
+        pile.spawn(connect, site)
 
+    # now that we've got the data, count it up
     distribution = defaultdict(int)
     for time in pile:
         distribution[time] += 1
@@ -126,7 +233,7 @@ def main(site):
 
     with open("results.csv", "a") as f:
         w = csv.writer(f)
-        w.writerow(["", "", "", "", "", "", "", "",])
+        w.writerow(["", "", "", "", "", "", "", ""])
         w.writerow([site, datetime.now().isoformat(), NUM_REQUESTS, CONCURRENT_REQUESTS])
         w.writerow(["num_seconds", "num_requests"])
         for t, v in zip(ticks, values):
